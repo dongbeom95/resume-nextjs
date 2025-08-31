f0a3524657b44f7308dcd61108150862e62216b9
dongbeom.kim <dongbeom.kim@opsnow.com>
2025-08-31 21:37:28 +0900

    init

--- payload/etc.ts
+++ payload/etc.ts
@@ -3,44 +3,53 @@ import { IEtc } from '../component/etc/IEtc';
 const etc: IEtc.Payload = {
   disable: false,
 
-  list: [
-    {
-      title: 'Snowflake SnowPro Core Certification',
-      startedAt: '2024-12',
-    },
-    {
-      title: 'Certified SAFe 6 Scrum Master',
-      startedAt: '2023-05',
-    },
-    {
-      title: 'Certified SAFe 6 PO/PM',
-      startedAt: '2023-03',
-    },
-    {
-      title: 'AWS Certified Solutions Architect - Professional',
-      startedAt: '2021-02',
-    },
-    {
-      title: 'GCP Professional Cloud Architect',
-      startedAt: '2020-06',
-    },
-    {
-      title: 'AWS Certified Solutions Architect - Associate',
-      startedAt: '2020-03',
-    },
-    {
-      title: 'AWS Certified Cloud Practitioner - Foundational',
-      startedAt: '2020-02',
-    },
-    {
-      title: 'SQLD (SQL Developer)',
-      startedAt: '2019-09',
-    },
-    {
-      title: '정보처리기사 (Engineer Information Processing)',
-      startedAt: '2019-05',
-    },
-  ],
+    list: [
+      {
+        title: 'Snowflake SnowPro Core Certification',
+        subTitle: 'Snowflake',
+        startedAt: '2024-12',
+      },
+      {
+        title: 'Certified SAFe 6 Scrum Master',
+        subTitle: 'Scaled Agile, Inc.',
+        startedAt: '2023-05',
+      },
+      {
+        title: 'Certified SAFe 6 PO/PM',
+        subTitle: 'Scaled Agile, Inc.',
+        startedAt: '2023-03',
+      },
+      {
+        title: 'AWS Certified Solutions Architect - Professional',
+        subTitle: 'Amazon Web Services',
+        startedAt: '2021-02',
+      },
+      {
+        title: 'GCP Professional Cloud Architect',
+        subTitle: 'Google Cloud',
+        startedAt: '2020-06',
+      },
+      {
+        title: 'AWS Certified Solutions Architect - Associate',
+        subTitle: 'Amazon Web Services',
+        startedAt: '2020-03',
+      },
+      {
+        title: 'AWS Certified Cloud Practitioner - Foundational',
+        subTitle: 'Amazon Web Services',
+        startedAt: '2020-02',
+      },
+      {
+        title: 'SQLD (SQL Developer)',
+        subTitle: 'Korea Data Agency',
+        startedAt: '2019-09',
+      },
+      {
+        title: '정보처리기사 (Engineer Information Processing)',
+        subTitle: 'HRD Korea',
+        startedAt: '2019-05',
+      },
+    ],
 };
 
 export default etc;
